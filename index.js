7d29f6539da746a7c1443f084387b87c492b2c08
unknown <leenuri1031@gmail.com>
2022-07-15 17:33:58 +0900

    main_page_javascipt_ver_complete

---
+++ index.js
@@ -0,0 +1,5 @@
+import mainSection from "./module/script/section_component.js"
+
+const container = document.getElementById('container');
+// console.log(container);
+container.innerHTML = mainSection;
